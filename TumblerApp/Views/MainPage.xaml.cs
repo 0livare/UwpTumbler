def7fbc3971b4360920c56b11b88583ae94741f1
Zach Posten <Zach.Posten@jci.com>
2017-11-07 09:13:49 -0600

    Add ruler lines

--- TumblerApp/Views/MainPage.xaml.cs
+++ TumblerApp/Views/MainPage.xaml.cs
@@ -1,4 +1,10 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+using TumblerApp.Util;
 
 namespace TumblerApp.Views
 {
@@ -7,6 +13,55 @@ namespace TumblerApp.Views
         public MainPage()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            double totalHeight = 700;
+            CreateRulerLines(totalHeight);
+        }
+
+        private void CreateRulerLines(double totalHeight)
+        {
+            for (var i = 0; i >= -totalHeight; i -= 100)
+            {
+                Relative.Children.Add(CreateRulerLine(i, totalHeight));
+            }
+
+            // Always add line for zero 
+            Relative.Children.Add(CreateRulerLine((int)totalHeight / 2 * -1, totalHeight));
+        }
+
+        private UIElement CreateRulerLine(int offset, double totalHeight)
+        {
+            var line = new Line
+            {
+                X2 = 500,
+                Stroke = new SolidColorBrush(Colors.Blue),
+                StrokeThickness = 2,
+                VerticalAlignment = VerticalAlignment.Bottom,
+            };
+
+            var text = new TextBlock
+            {
+                Text = "" + (offset + totalHeight / 2),
+                Margin = new Thickness(0, 0, 10, 0),
+                VerticalAlignment = VerticalAlignment.Bottom,
+            };
+
+            var sp = new StackPanel
+            {
+                Orientation = Orientation.Horizontal,
+                RenderTransform = new TranslateTransform { Y = offset },
+                VerticalAlignment = VerticalAlignment.Bottom,
+            };
+
+            sp.Children.Add(line);
+            sp.Children.Add(text);
+            RelativePanel.SetAlignBottomWith(sp, LoopItemsSample);
+
+            return sp;
         }
     }
 }
